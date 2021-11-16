4f4bab0c3a5c34f9172a04504ab9740d356e4d85
Benjamin Pollack <benjamin@bitquabit.com>
2021-11-16 17:39:05 -0500

    working

--- dortune.ts
+++ dortune.ts
@@ -1,1 +1,6 @@
-console.log("Yo dawg");
+#!/usr/bin/env deno run --allow-read
+
+const cleanFortunes = Deno.readTextFileSync("freebsd.fortunes");
+const fortunes = cleanFortunes.split("%").slice(1);
+
+console.log(fortunes[Math.floor(Math.random() * fortunes.length)].trim());

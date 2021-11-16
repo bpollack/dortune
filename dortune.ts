2942dc68735c86b49b9f0c837feca5da342e7911
Benjamin Pollack <benjamin@bitquabit.com>
2021-11-16 17:49:24 -0500

    add offensive flag, tighten perms

--- dortune.ts
+++ dortune.ts
@@ -1,6 +1,11 @@
-#!/usr/bin/env deno run --allow-read
+#!/usr/bin/env deno run --allow-read=.
 
 const cleanFortunes = Deno.readTextFileSync("freebsd.fortunes");
 const fortunes = cleanFortunes.split("%").slice(1);
+
+if (Deno.args.some((a) => a == "-o" || a == "--offensive")) {
+  const dirtyFortunes = Deno.readTextFileSync("offensive.fortunes");
+  fortunes.push(...dirtyFortunes.split("%").slice(1));
+}
 
 console.log(fortunes[Math.floor(Math.random() * fortunes.length)].trim());
